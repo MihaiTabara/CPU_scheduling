1962870b5ccc5185bc47597c340de30b79c2bd26
Ema <emma.ciobanu@gmail.com>
2013-04-28 18:47:32 +0300

    runner.sh: Updated file with CPU load parser
    Signed-off-by: Ema <emma.ciobanu@gmail.com>

--- runner.sh
+++ runner.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-mem_generator="PATH_TO/mem_parser.py"
+time_and_date='date -u +%D_%T'
+mem_generator="mem_parser.py"
+cpu_generator="cpu_parser.py"
 mem_runner="python $mem_generator"
-time_and_date='date -u'
+cpu_runner="python $cpu_generator"
 
 mem_load_arg="mem_load"
 cpu_load_arg="cpu_load"
 
+data=`eval $time_and_date`
+
 print_to_file_memory_stats() {
-  data=`eval $time_and_date`
   info=`eval $mem_runner`
   echo "$data $info" >> $1
 }
 
-if [ $# -eq 1 ]
+print_to_file_cpu_stats() {
+  eval $cpu_runner $1 $2
+}
+
+if [ $# -eq 2 ]
 then
   print_to_file_memory_stats $1
-  # TODO Ema - add the cpu load function
+  print_to_file_cpu_stats $2 $data
   exit
 else
   echo 'Usage: $0 $mem_load_arg $cpu_load_arg'
